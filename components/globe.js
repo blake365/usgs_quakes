b2295d8ae672551f241d74bc70dbe6475e4eefca
blake365 <68243611+blake365@users.noreply.github.com>
2024-11-30 15:44:01 -0500

    mobile globe fixes

--- components/globe.js
+++ components/globe.js
@@ -1,20 +1,32 @@
 import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
 
-// https://github.com/shuding/cobe
-
 export default function Globe({markers}) {
   const canvasRef = useRef();
 
-//   console.log(markers)
-
   useEffect(() => {
+    let width = 0;
     let phi = 0;
+
+    const onResize = () => {
+      if (canvasRef.current) {
+        const containerSize = Math.min(
+          window.innerWidth * 0.8,
+          window.innerHeight * 0.7
+        );
+        width = Math.min(600, containerSize);
+        canvasRef.current.style.width = `${width}px`;
+        canvasRef.current.style.height = `${width}px`;
+      }
+    }
+
+    window.addEventListener('resize', onResize);
+    onResize();
 
     const globe = createGlobe(canvasRef.current, {
       devicePixelRatio: 2,
-      width: 600 * 2,
-      height: 600 * 2,
+      width: width * 2,
+      height: width * 2,
       phi: 0,
       theta: 0,
       dark: 1,
@@ -25,24 +37,39 @@ export default function Globe({markers}) {
       markerColor: [0.9, 0.3, 0.5],
       glowColor: [1, 1, 1],
       markers: markers,
+      opacity: 0.8,
       onRender: (state) => {
-        // Called on every animation frame.
-        // `state` will be an empty object, return updated params.
+        state.width = width * 2;
+        state.height = width * 2;
         state.phi = phi;
         phi += 0.006;
       }
     });
 
+    setTimeout(() => {
+      if (canvasRef.current) {
+        canvasRef.current.style.opacity = "1";
+      }
+    });
+
     return () => {
       globe.destroy();
+      window.removeEventListener('resize', onResize);
     };
   }, [markers]);
 
   return (
-    <div className='flex items-center justify-center w-full h-full'>
+    <div className="flex items-center justify-center w-full p-4">
       <canvas
         ref={canvasRef}
-        style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
+        style={{
+          width: "100%",
+          height: "100%",
+          maxWidth: "600px",
+          aspectRatio: "1/1",
+          opacity: 0,
+          transition: "opacity 1s ease-in-out"
+        }}
       />
     </div>
   );
